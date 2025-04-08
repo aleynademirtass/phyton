7c388fc1d941588fbc491588da525a37b56834c2
aleynademirtass <aleynademirtas45@icloud.com>
2025-04-08 13:03:45 +0300

    commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(_name_)
 
 @app.route('/')
 def home():
-    return "deneme1234 fffaada bbbdd"
+    return "Aleyna Demirtaş"
 
 if _name_ == '_main_':
     app.run(host='0.0.0.0', port=80)
@@ -13,4 +13,4 @@ if _name_ == '_main_':
 
 # Uygulamayı Docker ile çalıştırmak için terminalde sırasıyla şu komutları çalıştır:
 # docker build -t  flask-deneme .
-# docker run -d -p 5050:80 --name flask-test flask-deneme
+# docker run -d -p 5055:80 --name flask-test flask-deneme
